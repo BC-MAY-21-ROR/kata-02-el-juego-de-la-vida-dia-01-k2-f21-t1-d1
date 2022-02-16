92c3e986b85e58d14bf63b902d1a01af0a33432c
Alejandro Curiel <alex_curielg@hotmail.com>
2022-02-15 19:22:40 -0600

    Add method of finding if cell is alive or dead
    Co-authored-by: HeikelV <heikelvillar@gmail.com>
    Co-authored-by: AlejandroCuevasZepeda <acuevas@ucol.mx>

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 line_num = 0
-celulas = []
+contenedor_celulas = []
 sizes = []
 text = File.open('cells.txt').read
 text.each_line do |line|
@@ -9,11 +9,26 @@ text.each_line do |line|
   if line_num == 2
     sizes.push(line.strip.split(' '))
   elsif line_num >= 3
-    celulas.push(line.strip.split(''))
+    contenedor_celulas.push(line.strip.split(''))
   end
 end
 
 x = sizes[0]
 y = sizes[1]
 
-print celulas
+print contenedor_celulas
+
+contenedor_celulas.each do |celulas|
+  celulas.each do |celula|
+    if(status_celula(celula) == true)
+      
+    end
+    else
+
+    end
+  end
+end
+
+def status_celula(celula)
+  celula=='*' ? true : false
+end
